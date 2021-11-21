a5249cd8f204325b1a32c6c5728f1c8c9fba859b
Adebayo Abdulwaasi <wasiuadekunle73@gmail.com>
2021-11-21 04:28:08 +0100

    stripe submenu final app

---
+++ setup/src/Sidebar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import sublinks from "./data";
+import { FaTimes } from "react-icons/fa";
+import { useGlobalContext } from "./context";
+const Sidebar = () => {
+  const { closeSidebar, isSidebarOpen } = useGlobalContext();
+  return (
+    <div
+      className={`${
+        isSidebarOpen ? "sidebar-wrapper show" : "sidebar-wrapper"
+      }`}
+    >
+      <aside className="sidebar">
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+        <div className="sidebar-links">
+          {sublinks.map((items, index) => {
+            const { page, links } = items;
+            return (
+              <article key={index}>
+                <h4>{page}</h4>
+                <div className="sidebar-sublinks">
+                  {links.map((link, index) => {
+                    const { label, icon, url } = link;
+                    return (
+                      <a href={url} key={index}>
+                        {icon}
+                        {label}
+                      </a>
+                    );
+                  })}
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      </aside>
+    </div>
+  );
+};
+export default Sidebar;
